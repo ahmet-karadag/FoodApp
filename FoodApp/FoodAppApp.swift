61dbb68f9335364ccc34ba973e34c5a7aa1b096b
Ahmet Karadağ <akkaradaga@gmail.com>
2023-12-02 22:37:17 +0300

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by ahmet karadağ on 2.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
